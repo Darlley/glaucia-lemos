fbc32671223f4448aa0be6778775d4a03535098a
Darlley <darlleybrito@gmail.com>
2021-08-06 18:37:59 -0400

    first commit

---
+++ projeto-2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- INICIO NAVBAR -->
+    <nav class="navbar navbar-expand-lg sticky-top bg-dark navbar-dark">
+      <div class="container d-flex justify-content-lg-around">
+          <a href="" class="navbar-brand">CRUD Vue.js + PostgreeSQL + Node.js</a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Create New Employee</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/edit-employee/:id">Edit Employee</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/list-employee">List all Employees</router-link>
+            </li>
+          </ul>
+      </div>
+    </nav>
+    <!-- FIM NAVBAR -->
+    <div class="container mt-5">
+      <router-view />
+    </div>
+  </div>
+</template>
---
+++ projeto-2/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Create New Employee',
+    component: () => import('../components/create-employee/CreateEmployeeComponent.vue'),
+  },
+  {
+    path: '/list-employee',
+    name: 'List All Employees',
+    component: () => import('../components/list-employee/ListEmployeeComponent.vue'),
+  },
+  {
+    path: '/edit-employee/:id',
+    name: 'Update Employee',
+    component: () => import('../components/edit-employee/EditEmployeeComponent.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
